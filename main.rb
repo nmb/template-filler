dcf6da55b58afbc5e22b72adfe66a5226e5c8621
Mikael Borg <mikael.borg@nbis.se>
2021-09-25 11:46:17 +0200

    Fix scoping issue. Improve helptext.

--- main.rb
+++ main.rb
@@ -2,47 +2,56 @@
 require 'libui'
 require './fill.rb'
 
+UI = LibUI
+
+def main
 @template_file = ""
 @data_file = ""
 @suffixes = []
 @suffix = ""
+@cbox
 
 help_text = %{
-This program takes a word template and an excel file, and creates a new word file for each data row.
+This program takes a template (word file) and a data source (excel file), and
+creates a new word file for each data row.
 
-The template must use MailMerge fields as markers. The data file should contain column headers on the first row.
+The template must use MailMerge fields as markers. The data file should contain
+column headers on the first row, corresponding to the MailMerge field names.
 
-After a data file has been loaded, it is possible to select a header to use as suffix for the resulting files.
-The files are created in the same folder as the template, with file names [template name]-[suffix].
+After a data file has been loaded, it is possible to select a header to use as
+suffix for the resulting files.  
+
+The files are created in the same folder as the template, with file names
+[template name]-[suffix].
 }
 about_text = %{
 Source code available at https://github.com/nmb/template-filler .
 }
-UI = LibUI
+
 UI.init
 
 # Add help menu
 menu = UI.new_menu('Help')
 help_menu_item = UI.menu_append_item(menu, 'Instructions')
 UI.menu_item_on_clicked(help_menu_item) do
-    UI.msg_box(MAIN_WINDOW, 'Instructions', help_text)
+    UI.msg_box(@main_window, 'Instructions', help_text)
 end
 about_menu_item = UI.menu_append_item(menu, 'About')
 UI.menu_item_on_clicked(about_menu_item) do
-    UI.msg_box(MAIN_WINDOW, 'About', about_text)
+    UI.msg_box(@main_window, 'About', about_text)
 end
 UI.menu_append_quit_item(menu)
 UI.on_should_quit(proc {
-  UI.control_destroy(MAIN_WINDOW)
+  UI.control_destroy(@main_window)
   UI.quit
   0
 })
 
 # Set up main window
-MAIN_WINDOW = UI.new_window('Fill Template', 200, 100, 1)
-UI.window_set_margined(MAIN_WINDOW, 1)
+@main_window = UI.new_window('Fill Template', 200, 100, 1)
+UI.window_set_margined(@main_window, 1)
 vbox = UI.new_vertical_box
-UI.window_set_child(MAIN_WINDOW, vbox)
+UI.window_set_child(@main_window, vbox)
 hbox = UI.new_horizontal_box
 UI.box_set_padded(vbox, 1)
 UI.box_set_padded(hbox, 1)
@@ -60,13 +69,13 @@ UI.box_set_padded(inner, 1)
 UI.group_set_child(group, inner)
 
 UI.button_on_clicked(template_button) do
-  str = UI.open_file(MAIN_WINDOW)
+  str = UI.open_file(@main_window)
   @template_file = str.to_s unless str.null?
 
 end
 
 UI.button_on_clicked(data_button) do
-  str = UI.open_file(MAIN_WINDOW)
+  str = UI.open_file(@main_window)
   unless str.null?
     @data_file = str.to_s
     @suffix = ""
@@ -74,14 +83,14 @@ UI.button_on_clicked(data_button) do
     inner = UI.new_vertical_box
     UI.box_set_padded(inner, 1)
     UI.group_set_child(group, inner)
-    cbox = UI.new_combobox
+    @cbox = UI.new_combobox
     headers = Roo::Spreadsheet.open(@data_file).sheet(0).parse(headers: true, clean: true).shift
     @suffixes = headers.keys
     @suffixes.each do |h|
-      UI.combobox_append(cbox, h)
+      UI.combobox_append(@cbox, h)
     end
-    UI.box_append(inner, cbox, 0)
-    UI.combobox_on_selected(cbox) do |ptr|
+    UI.box_append(inner, @cbox, 0)
+    UI.combobox_on_selected(@cbox) do |ptr|
       no = UI.combobox_selected(ptr)
       @suffix = @suffixes[no]
 end
@@ -92,12 +101,12 @@ end
 UI.button_on_clicked(go_button) do
   if(File.exist?(@template_file) && File.exist?(@data_file))
     fill_all(@data_file, @template_file, @suffix)
-    UI.msg_box(MAIN_WINDOW, 'Information', "Template: #{@template_file}.\nData: #{@data_file}.\nDone!")
+    UI.msg_box(@main_window, 'Information', "Template: #{@template_file}.\nData: #{@data_file}.\nDone!")
   end
 end
 
-UI.window_on_closing(MAIN_WINDOW) do
-  UI.control_destroy(MAIN_WINDOW)
+UI.window_on_closing(@main_window) do
+  UI.control_destroy(@main_window)
   UI.quit
   0
 end
@@ -107,7 +116,19 @@ UI.box_append(inner, @cbox, 0)
 UI.box_append(hbox, template_button, 0)
 UI.box_append(hbox, data_button, 0)
 UI.box_append(hbox, go_button, 0)
-UI.control_show(MAIN_WINDOW)
+UI.control_show(@main_window)
 
 UI.main
 UI.quit
+
+end
+
+begin
+  main unless defined?(Ocra)
+rescue Exception => e
+    f = Tempfile.new(['template-filler-', '.log']) 
+    f.puts e.inspect
+    f.puts e.backtrace
+    f.close
+end
+
