b62ff2a87f940a8dd12be61850d01d781915fdee
Mikael Borg <mikael.borg@nbis.se>
2021-09-25 11:39:47 +0200

    Convert file names to strings. Error handling.

--- fill.rb
+++ fill.rb
@@ -2,19 +2,25 @@ require 'roo'
 require 'sablon'
 
 def sanitize(filename)
+  res = String(filename)
   # Bad as defined by wikipedia: https://en.wikipedia.org/wiki/Filename#Reserved_characters_and_words
   # Also have to escape the backslash
   bad_chars = [ '/', '\\', '?', '%', '*', ':', '|', '"', '<', '>', '.', ' ' ]
   bad_chars.each do |bad_char|
-    filename.gsub!(bad_char, '_')
+    res.gsub!(bad_char, '_')
   end
-  filename
+  res
 end
 
 def fill(data, template, suffix, template_file)
   output_dir = File.dirname(template_file)
   base = File.basename(template_file, '.docx')
-  template.render_to_file(File.join(output_dir, base + suffix + '.docx'), data)
+  filename = File.join(output_dir, base + suffix + '.docx')
+  begin
+  template.render_to_file(filename, data)
+  rescue
+    p "error"
+  end
 end
 
 def fill_all(datasheet, template_file, suffix_key = nil)
